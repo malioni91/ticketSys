f7e940d6ee3eac8ade0eeb000601327c5a8878c5
Malion Hoxhallari <malioni91@gmail.com>
2017-05-29 01:28:48 +0100

    first commit

---
+++ tickets/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+# Create your models here.
+
+class Ticket(models.Model):
+    
+    DYNAX = 'DAX'
+    DOTNET = 'Net'
+    SQL = 'sql'
+    DEVELOPMENT_CATEGORY = (
+        (DYNAX, 'DynAX'),
+        (DOTNET ,'DotNet'),
+        (SQL, 'Sql')
+    ) 
+    title = models.CharField(max_length=60)
+    description = models.TextField()
+    user_assigned = models.ForeignKey(User, on_delete=models.CASCADE)
+    pub_date = models.DateField(auto_now_add=False)
+    upd_date = models.DateField(auto_now=False)
+    category = models.CharField(
+        max_length=3,
+        choices=DEVELOPMENT_CATEGORY,
+        default=DYNAX,
+    )
+    completed = models.BooleanField()
+    days_allocated = models.IntegerField(default=1)
+    
+
+
+class 
